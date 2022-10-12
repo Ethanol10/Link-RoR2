a95e726f23d445ad37b7b6b5e4d764dd9f7a81ea
Ethanol10 <35375774+Ethanol10@users.noreply.github.com>
2022-10-13 00:44:37 +1100

    implementing aerial spinattack start

--- LinkMod/SkillStates/Link/MasterSwordSpinAttack/AerialSpinAttack.cs
+++ LinkMod/SkillStates/Link/MasterSwordSpinAttack/AerialSpinAttack.cs
@@ -8,10 +8,23 @@ namespace LinkMod.SkillStates.Link.MasterSwordSpinAttack
 {
     internal class AerialSpinAttack : BaseSkillState
     {
+        public static float duration = 1.5f;
+        public static float initialSpeedCoefficient = 5f;
+        public static float finalSpeedCoefficient = 0f;
+
+        private float rollSpeed;
+        private Vector3 upwardDirection;
+        private Animator animator;
+        private Vector3 previousPosition;
+
         public override void OnEnter()
         {
             base.OnEnter();
-       }
+            animator = base.GetModelAnimator();
+            animator.SetFloat("Swing.playbackRate", 1.0f);
+
+            base.PlayAnimation("FullBody, Override", "SpinAttackAir", "Swing.playbackRate", duration);
+        }
 
         public override void OnExit()
         {
